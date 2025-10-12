2df333d6c3f7ed94aa6b6d05490a6399dd77adc1
julinuzzo19 <julinuzzo19@gmail.com>
2025-10-11 22:41:51 -0300

    refactor: update Dockerfile for improved build process and dynamic port exposure; add Dockerfile.dev for development environment

--- src/shared/infrastructure/db/prisma/prisma.client.ts
+++ src/shared/infrastructure/db/prisma/prisma.client.ts
@@ -4,17 +4,20 @@ import { PrismaClient } from "@prisma/client";
  * Configuración de URL de base de datos según el entorno
  */
 const getDatabaseUrl = (): string => {
-  const nodeEnv = process.env.NODE_ENV || "development";
+  // const nodeEnv = process.env.NODE_ENV || "development";
 
-  switch (nodeEnv) {
-    case "production":
-      return process.env.DATABASE_URL || "";
-    case "test":
-      return process.env.DATABASE_URL_TEST || "";
-    case "development":
-    default:
-      return process.env.DATABASE_URL_DEV || "";
-  }
+  const URL = process.env.DATABASE_URL as string;
+
+  return URL;
+  // switch (nodeEnv) {
+  //   case "production":
+  //     return process.env.DATABASE_URL || "";
+  //   case "test":
+  //     return process.env.DATABASE_URL_TEST || "";
+  //   case "development":
+  //   default:
+  //     return process.env.DATABASE_URL_DEV || "";
+  // }
 };
 
 /**
