0a66c0ed7cec3020fa1cc13b885480aebb0f7e46
julinuzzo19 <julinuzzo19@gmail.com>
2025-11-01 16:02:08 +0000

    add script and test config

--- tests/setup/jest.config.cjs
+++ tests/setup/jest.config.cjs
@@ -6,9 +6,10 @@ module.exports = {
       testEnvironment: 'node',
       rootDir: process.cwd(),
       testMatch: [
-        '<rootDir>/src/**/domain/__tests__/**/*.test.ts',
-        '<rootDir>/src/**/application/__tests__/**/*.test.ts',
-        '<rootDir>/src/shared/**/__tests__/**/*.test.ts',
+        '<rootDir>/src/**/domain/__tests__/unit/**/*.test.ts',
+        '<rootDir>/src/**/application/__tests__/unit/**/*.test.ts',
+        '<rootDir>/src/**/infrastructure/__tests__/unit/**/*.test.ts',
+        '<rootDir>/src/shared/**/__tests__/unit/**/*.test.ts',
       ],
       moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
@@ -39,7 +40,8 @@ module.exports = {
       testEnvironment: 'node',
       rootDir: process.cwd(),
       testMatch: [
-        '<rootDir>/src/**/infrastructure/__tests__/**/*.integration.test.ts',
+        '<rootDir>/src/**/application/__tests__/integration/**/*.test.ts',
+        '<rootDir>/src/**/infrastructure/__tests__/integration/**/*.test.ts',
       ],
       setupFilesAfterEnv: ['<rootDir>/tests/setup/testDatabase.ts'],
       moduleNameMapper: {
@@ -70,10 +72,7 @@ module.exports = {
       preset: 'ts-jest/presets/default-esm',
       testEnvironment: 'node',
       rootDir: process.cwd(),
-      testMatch: [
-        '<rootDir>/src/**/infrastructure/__tests__/**/*.e2e.test.ts',
-        '<rootDir>/tests/e2e/**/*.test.ts',
-      ],
+      testMatch: ['<rootDir>/src/**/infrastructure/__tests__/e2e/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/tests/setup/testDatabase.ts'],
       moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
