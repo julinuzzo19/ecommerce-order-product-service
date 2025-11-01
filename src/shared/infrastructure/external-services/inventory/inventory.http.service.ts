af85cb4066f61749258209a861d3f6f6443382e1
julinuzzo19 <julinuzzo19@gmail.com>
2025-11-01 16:02:29 +0000

    fix inventory service config

--- src/shared/infrastructure/external-services/inventory/inventory.http.service.ts
+++ src/shared/infrastructure/external-services/inventory/inventory.http.service.ts
@@ -9,14 +9,15 @@ export class InventoryHttpService implements IInventoryService {
   private client: AxiosInstance;
 
   constructor() {
+    console.log({ gateway: process.env.GATEWAY_SECRET });
     // Configuración del cliente HTTP
     this.client = axios.create({
       //   baseURL: process.env.INVENTORY_SERVICE_URL,
-      baseURL: 'http://ecommerce-inventory-service:3011/api/v1/inventory',
+      baseURL: process.env.GATEWAY_SERVICE + '/inventory',
       timeout: 5000,
       headers: {
         'Content-Type': 'application/json',
-        // 'Authorization': `Bearer ${process.env.INVENTORY_SERVICE_TOKEN}`
+        'x-gateway-secret': process.env.GATEWAY_SECRET,
       },
     });
   }
@@ -26,6 +27,7 @@ export class InventoryHttpService implements IInventoryService {
   ): Promise<InventoryCheckResult> {
     try {
       const response = await this.client.post('/check-stock', items);
+      console.log({ response });
       return response.data as InventoryCheckResult;
     } catch (error) {
       throw new Error(
